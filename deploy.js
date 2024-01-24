4ef44965be61db95276127b8339702e1e40b342e
navin <navinrmeena13@gmail.com>
2024-01-25 01:51:16 +0530

    over basics

--- deploy.js
+++ deploy.js
@@ -11,9 +11,24 @@ async function main() {
   // compile them separately
   // http://127.0.0.1:7545
   const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+
+
   // const provider = new ethers.providers.Web3Provider(ganache.provider());
-  // signer = await provider.getSigner();
+  // signer = await provider.getSigner();x
+
+  const encryptedJson = fs.readFileSync("./encryptedJsonKey.json", "utf8");
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+
+// ************************ when we are using encpted private key in json format *************************
+  // let wallet = ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PRIVATE_KEY_PASSWORD
+  // );
+  // wallet = wallet.connect(provider);
+// ***********************$$$$$$$$$$$$$$$******************************
+
+
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
